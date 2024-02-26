41206852a8dae22d9f721464db742ba13328432d
dylanxhernandez <dylan.x.h.developer@gmail.com>
2024-02-26 17:49:24 -0500

    feat: add view and routing for basic markdown content page

--- proto_dmd_app/views.py
+++ proto_dmd_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import MarkdownContent
 
-# Create your views here.
+def markdown_content_view(request):
+    markdown_content = MarkdownContent.objects.first()
+    context = {"markdown_content": markdown_content}
+    return render(
+        request,
+        "proto_dmd_app/markdown_content.html",
+        context=context
+    )
