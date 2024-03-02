a035ee479ccfc5842eb22c3e4cb268155d4bb475
dylanxhernandez <dylan.x.h.developer@gmail.com>
2024-03-02 09:01:12 -0500

    feat: add slug field to markdown data model

--- proto_dmd_app/models.py
+++ proto_dmd_app/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class MarkdownContent(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    slug = models.SlugField(blank=True)
 
     class Meta:
         verbose_name_plural = "Markdown content"
