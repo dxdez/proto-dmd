210b685c39c9ca36f046ad9acde9a30703748896
dylanxhernandez <dylan.x.h.developer@gmail.com>
2024-03-11 13:09:46 -0400

    feat: add index routing for main page boilerplate

--- proto_dmd_app/views.py
+++ proto_dmd_app/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from .models import MarkdownContent
 
+def index_view(request):
+    context = {"content": {"title":"Index", "content":"This the the homepage"}}
+    return render(
+        request,
+        "proto_dmd_app/index_template.html",
+        context=context
+    )
+
 def markdown_content_view(request, slug):
     markdown_content = get_object_or_404(MarkdownContent, slug=slug)
     context = {"markdown_content": markdown_content}
--- proto_dmd_project/urls.py
+++ proto_dmd_project/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from proto_dmd_app.views import markdown_content_view
+from proto_dmd_app.views import index_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", index_view, name="index"),
     path(
-        "markdown-content/<slug:slug>/", markdown_content_view, name="markdown-content"
+        "mc/<slug:slug>/", markdown_content_view, name="markdown-content"
     ),
 ]
 
