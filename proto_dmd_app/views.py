3906b40a36b5499599a464b2ba1085e3b465c883
dylanxhernandez <dylan.x.h.developer@gmail.com>
2024-04-05 13:54:50 -0400

    feat: add basic form for markdown record creation

--- proto_dmd_app/views.py
+++ proto_dmd_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
 from .models import MarkdownContent
+from .forms import MarkdownForm
 
 def index_view(request):
     markdown_records = MarkdownContent.objects.order_by('-id')[:10]
@@ -26,3 +27,23 @@ def markdown_content_view(request, slug):
         "proto_dmd_app/markdown_content.html",
         context=context
     )
+
+def markdown_create(request):
+    if request.method == 'POST':
+        form = MarkdownForm(request.POST)
+        if form.is_valid():
+            form.save()    
+            return HttpResponse('<p>Info Saved!</p>')
+        else:
+            return HttpResponse('<p>Info is not Valid</p>')
+    else:
+        form = MarkdownForm
+        context = {
+            'form': form
+        }
+        return render(
+            request,
+            "proto_dmd_app/markdown_create.html",
+            context=context
+        )
+        
