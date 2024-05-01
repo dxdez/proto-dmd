5c78b199c20b8acf8efa91452142360e05fecee8
dylanxhernandez <dylan.x.h.developer@gmail.com>
2024-05-01 13:35:46 -0400

    feat: add error response for invalid forms

--- proto_dmd_app/views.py
+++ proto_dmd_app/views.py
@@ -5,16 +5,16 @@ from .forms import MarkdownForm
 def index_view(request):
     markdown_records = MarkdownContent.objects.order_by('-id')[:10]
     context = {
-        'content': {
-            'title': 'Home',
-            'markdown_list': markdown_records
-        }
-    }
+            'content': {
+                'title': 'Home',
+                'markdown_list': markdown_records
+                }
+            }
     return render(
-        request,
-        'proto_dmd_app/index.html',
-        context=context
-    )
+            request,
+            'proto_dmd_app/index.html',
+            context=context
+            )
 
 def redirect_index_view(request):
     return redirect('/')
@@ -23,30 +23,40 @@ def markdown_content_view(request, slug):
     markdown_content = get_object_or_404(MarkdownContent, slug=slug)
     context = {'markdown_content': markdown_content}
     return render(
-        request,
-        'proto_dmd_app/markdown_content.html',
-        context=context
-    )
+            request,
+            'proto_dmd_app/markdown_content.html',
+            context=context
+            )
 
 def markdown_create(request):
     if request.method == 'POST':
         form = MarkdownForm(request.POST)
         if form.is_valid():
             form.save()    
-            return HttpResponse('<p>Info Saved!</p>')
+            return redirect('/')
         else:
-            return HttpResponse('<p>Info is not Valid</p>')
+            context = {
+                    'form': form,
+                    'operation': 'Create'
+                    }
+            response = render(
+                    request, 
+                    'proto_dmd_app/markdown_form.html', 
+                    context=context
+                    ) 
+            response.status_code = 400
+            return response
     else:
         form = MarkdownForm
         context = {
-            'form': form,
-            'operation': 'Create'
-        }
+                'form': form,
+                'operation': 'Create'
+                }
         return render(
-            request,
-            'proto_dmd_app/markdown_form.html',
-            context=context
-        )
+                request,
+                'proto_dmd_app/markdown_form.html',
+                context=context
+                )
 
 def markdown_edit(request, id):
     edit_markdown_content = get_object_or_404(MarkdownContent, pk=id)
@@ -54,18 +64,28 @@ def markdown_edit(request, id):
         form = MarkdownForm(request.POST, instance=edit_markdown_content)
         if form.is_valid():
             form.save()    
-            return HttpResponse('<p>Info Updated!</p>')
+            return redirect('/')
         else:
-            return HttpResponse('<p>Info is not Valid</p>')
+            context = {
+                    'form': form,
+                    'operation': 'Edit'
+                    }
+            response = render(
+                    request, 
+                    'proto_dmd_app/markdown_form.html', 
+                    context=context
+                    ) 
+            response.status_code = 400
+            return response
     else:
         form = MarkdownForm(instance=edit_markdown_content)
         context = {
-            'form': form,
-            'operation': 'Edit'
-        }
+                'form': form,
+                'operation': 'Edit'
+                }
         return render(
-            request,
-            'proto_dmd_app/markdown_form.html',
-            context=context
-        )
+                request,
+                'proto_dmd_app/markdown_form.html',
+                context=context
+                )
 
