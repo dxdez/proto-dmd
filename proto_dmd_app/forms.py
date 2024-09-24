e4939bdb9a06a8f1d4a959e03e85a7aa209f6a0e
dylanxhernandez <dylan.x.h.developer@gmail.com>
2024-09-24 15:15:58 -0400

    feat: add field style classes to form

--- proto_dmd_app/forms.py
+++ proto_dmd_app/forms.py
@@ -1,7 +1,18 @@
+from django import forms
 from django.forms import ModelForm
 from .models import MarkdownContent
 
 class MarkdownForm(ModelForm):
+    title = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'w-full px-3 py-2 border rounded-lg'})
+    )
+    content = forms.CharField(
+        widget=forms.Textarea(attrs={'class': 'w-full px-3 py-2 border rounded-lg', 'rows': 10})
+    )
+    slug = forms.SlugField(
+        widget=forms.TextInput(attrs={'class': 'w-full px-3 py-2 border rounded-lg'})
+    )
+
     class Meta:
         model = MarkdownContent
         fields = '__all__'
